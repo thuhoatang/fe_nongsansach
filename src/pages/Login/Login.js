6484bea4fa787814c6ea80b469934b1b7fc04d38
hoatangthithu <hoa.tangthithu@ncc.asia>
2023-01-05 21:28:41 +0700

    Giao diện Đăng nhập

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,48 +1,76 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux';
-import { signInAction } from '../../actions/authAction';
+import React, { useState } from "react";
+import "./Login.css";
+import { connect } from "react-redux";
+import { signInAction } from "../../actions/authAction";
 
 const Login = ({ signInAction, auth }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    const submit = async () => {
+      signInAction({ username: username, password: password });
+    };
 
-    const onFormSubmit = (e) => {
-        e.preventDefault();
-        const submit = async () => {
-            signInAction({ username: username, password: password });
-        }
+    submit();
+  };
+  return (
+    <div className="container form-singin text-center">
+      <form onSubmit={onFormSubmit}>
+        <p className="title-form">ĐĂNG NHẬP</p>
+        <p className="dang-ki-tai-day">
+          Nếu bạn chưa có tài khoản, <a href="#">đăng kí tại đây!</a>
+        </p>
 
-        submit();
-    }
-    return (
-        <div className='container'>
-            <form onSubmit={onFormSubmit}>
-                <div id="emailHelp" className="form-text">{auth?.error}</div>
-
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input value={username} onChange={(e) => { setUsername(e.target.value) }} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" className="form-control" id="exampleInputPassword1"></input>
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-
+        <div id="emailHelp" className="form-text error">
+          {auth?.error}
         </div>
-    )
-}
+
+        <div className="mb-3">
+          <input
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            type="text"
+            className=""
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Tên đăng nhập"
+          ></input>
+        </div>
+        <div className="mb-3">
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            className=""
+            placeholder="Mật khẩu"
+            id="exampleInputPassword1"
+          ></input>
+        </div>
+
+        <button type="submit" className="btn-signin">
+          Đăng nhập
+        </button>
+      </form>
+
+      <div className="login-google">
+        <p className="my-3">Hoặc đăng nhập bằng</p>
+
+        <button className="btn-login-google">Google</button>
+      </div>
+    </div>
+  );
+};
 
 const mapstateToProps = (state) => {
-    return {
-        auth: state.auth
-    };
+  return {
+    auth: state.auth,
+  };
 };
 export default connect(mapstateToProps, { signInAction })(Login);
 // export default Login;
